4587344321972b273dc4f230cff34ac2e934d062
Timoty Granziero <timoty.granziero@gmail.com>
2021-06-01 14:04:48 +0200

    Rearrange test variables

--- src/test/kotlin/ScraperActualWebTest.kt
+++ src/test/kotlin/ScraperActualWebTest.kt
@@ -8,21 +8,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty
 import java.net.URL
 
-private val onePiece =
-    Comic(STAR_COMICS, URL("https://www.starcomics.com/fumetto/young-301-one-piece-90"), "One Piece 90")
-private val vinlandSaga =
-    Comic(STAR_COMICS, URL("https://www.starcomics.com/fumetto/action-208-vinland-saga-7"), "Vinland Saga 7")
-private val chainsawMan = Comic(
-    PANINI_COMICS,
-    URL("https://www.panini.it/shp_ita_it/chainsaw-man-4-mmost014-it08.html"),
-    "Chainsaw Man 4"
-)
-private val homunculus = Comic(
-    PANINI_COMICS,
-    URL("https://www.panini.it/shp_ita_it/homunculus-1-mmgno023isbnr4-it08.html"),
-    "Homunculus 1"
-)
-
 @EnabledIfSystemProperty(named = "webTest", matches = "true")
 internal class ScraperActualWebTest {
     private val fireNotificator = FakeFireNotificator()
@@ -42,3 +27,18 @@ internal class ScraperActualWebTest {
         assertTrue(fireNotificator.hasNotBeenNotified(homunculus))
     }
 }
+
+private val onePiece =
+    Comic(STAR_COMICS, URL("https://www.starcomics.com/fumetto/young-301-one-piece-90"), "One Piece 90")
+private val vinlandSaga =
+    Comic(STAR_COMICS, URL("https://www.starcomics.com/fumetto/action-208-vinland-saga-7"), "Vinland Saga 7")
+private val chainsawMan = Comic(
+    PANINI_COMICS,
+    URL("https://www.panini.it/shp_ita_it/chainsaw-man-4-mmost014-it08.html"),
+    "Chainsaw Man 4"
+)
+private val homunculus = Comic(
+    PANINI_COMICS,
+    URL("https://www.panini.it/shp_ita_it/homunculus-1-mmgno023isbnr4-it08.html"),
+    "Homunculus 1"
+)
